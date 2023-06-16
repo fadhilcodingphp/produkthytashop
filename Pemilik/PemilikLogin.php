7ff7325433b02b46a7d7b0e0dc874644a6ee089d
fadhilcodingphp <muhammadfadhilay22@gmail.com>
2023-06-17 02:27:03 +0800

    revisi

--- Pemilik/PemilikLogin.php
+++ Pemilik/PemilikLogin.php
@@ -45,7 +45,7 @@ if (isset($_POST['loginpemilik'])) {
             <div class="container">
                <div class="row justify-content-center">
                   <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                     <div class="d-flex justify-content-center py-4"> <a href="Dashboard.php" class="logo d-flex align-items-center w-auto"> <img src="assets/img/logo.png" alt=""> <span class="d-none d-lg-block">Pemilik Thytashop</span> </a></div>
+                     <h5 class="card-title text-center pb-0 fs-4 mb-4">Pemilik</h5>
                      <div class="card mb-3">
                         <div class="card-body">
                            <div class="pt-4 pb-2">
